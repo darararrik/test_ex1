b7db9a1bd708dddf9f938ea122bbeec464a63401
Damir <imyaminovdamir10@gmail.com>
2025-08-18 20:13:32 +0600

    fix: spider lib paths

--- lib/presentation/constants/app_icons.dart
+++ lib/presentation/constants/app_icons.dart
@@ -1,31 +1,38 @@
 class AppIcons {
   AppIcons._();
 
-  static const String arrow = 'assets/icons/arrow.svg';
-  static const String back = 'assets/icons/back.svg';
-  static const String background = 'assets/icons/background.svg';
-  static const String check = 'assets/icons/check.svg';
-  static const String checkSubscribe = 'assets/icons/check_subscribe.svg';
-  static const String close = 'assets/icons/close.svg';
-  static const String exit = 'assets/icons/exit.svg';
-  static const String eyeClosed = 'assets/icons/eye_closed.svg';
-  static const String eyeOpen = 'assets/icons/eye_open.svg';
-  static const String loadingLargeDark = 'assets/icons/loading_large_dark.svg';
+  static const String arrow = 'lib/presentation/assets/icons/arrow.svg';
+  static const String back = 'lib/presentation/assets/icons/back.svg';
+  static const String background =
+      'lib/presentation/assets/icons/background.svg';
+  static const String check = 'lib/presentation/assets/icons/check.svg';
+  static const String checkSubscribe =
+      'lib/presentation/assets/icons/check_subscribe.svg';
+  static const String close = 'lib/presentation/assets/icons/close.svg';
+  static const String exit = 'lib/presentation/assets/icons/exit.svg';
+  static const String eyeClosed =
+      'lib/presentation/assets/icons/eye_closed.svg';
+  static const String eyeOpen = 'lib/presentation/assets/icons/eye_open.svg';
+  static const String loadingLargeDark =
+      'lib/presentation/assets/icons/loading_large_dark.svg';
   static const String loadingLargeWhite =
-      'assets/icons/loading_large_white.svg';
+      'lib/presentation/assets/icons/loading_large_white.svg';
   static const String loadingMediumDark =
-      'assets/icons/loading_medium_dark.svg';
+      'lib/presentation/assets/icons/loading_medium_dark.svg';
   static const String loadingMediumWhite =
-      'assets/icons/loading_medium_white.svg';
-  static const String myDesk = 'assets/icons/my_desk.svg';
-  static const String nonetwork = 'assets/icons/nonetwork.svg';
-  static const String paperAirplane = 'assets/icons/paper_airplane.svg';
-  static const String plus = 'assets/icons/plus.svg';
-  static const String prayArms = 'assets/icons/pray_arms.svg';
-  static const String profile = 'assets/icons/profile.svg';
-  static const String search = 'assets/icons/search.svg';
-  static const String sketch = 'assets/icons/sketch.svg';
-  static const String subscribers = 'assets/icons/subscribers.svg';
-  static const String trash = 'assets/icons/trash.svg';
-  static const String usersDesks = 'assets/icons/users_desks.svg';
+      'lib/presentation/assets/icons/loading_medium_white.svg';
+  static const String myDesk = 'lib/presentation/assets/icons/my_desk.svg';
+  static const String nonetwork = 'lib/presentation/assets/icons/nonetwork.svg';
+  static const String paperAirplane =
+      'lib/presentation/assets/icons/paper_airplane.svg';
+  static const String plus = 'lib/presentation/assets/icons/plus.svg';
+  static const String prayArms = 'lib/presentation/assets/icons/pray_arms.svg';
+  static const String profile = 'lib/presentation/assets/icons/profile.svg';
+  static const String search = 'lib/presentation/assets/icons/search.svg';
+  static const String sketch = 'lib/presentation/assets/icons/sketch.svg';
+  static const String subscribers =
+      'lib/presentation/assets/icons/subscribers.svg';
+  static const String trash = 'lib/presentation/assets/icons/trash.svg';
+  static const String usersDesks =
+      'lib/presentation/assets/icons/users_desks.svg';
 }
